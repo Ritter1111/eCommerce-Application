d59674950df4d52542167c35c56c609418d3cab5
Angelina Liapin <angelina.liapin@gmail.com>
2023-08-02 02:33:57 +0300

    feat: configure webpack with eslint, prettier, typescript and react

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>
+    hjdss{' '}
+    <div>
+      dksjdkdivjjdks <div>jdshf</div>
+    </div>
+  </h1>,
+  document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const baseConfig = {
+  entry: path.resolve(__dirname, './src/index.tsx'),
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(js|ts)x$/,
+        use: { loader: 'babel-loader' },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(c|sa|sc)ss$/i,
+        exclude: /felipec\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx', '.jsx'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, './dist'),
+    assetModuleFilename: 'assets/[name][ext]',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './public/index.html'),
+      filename: 'index.html',
+    }),
+    new ESLintPlugin({ extensions: ['ts'] }),
+    new MiniCssExtractPlugin({
+      filename: 'style.[contenthash].css',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
+
+module.exports = ({ mode }) => {
+  const isProductionMode = mode === 'prod';
+  const envConfig = isProductionMode 
+    ? require('./webpack.prod.config')
+    : require('./webpack.dev.config');
+
+  return merge(baseConfig, envConfig);
+};
