bed43e892d0a69a5f3a92b3bf6593a8bd01e283f
Angelina Liapin <angelina.liapin@gmail.com>
2023-08-02 12:56:51 +0300

    fix: change configuration husky

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,19 @@ import React from 'react';
 const App = () => {
   return (
     <div>
-      <h1> <h4>fcdhdhf     <p>jdkjdk shdbf j        hds
-        
-        
-
-
-
-        <label htmlFor="dsj"></label     >
-        
-        </p></h4> Hello</h1>
+      <h1>
+        {' '}
+        <h4>
+          fcdhdhf{' '}
+          <p>
+            jdkjdk shdbf j hds
+            <label htmlFor="dsj"></label>
+          </p>
+        </h4>{' '}
+        Hello
+      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-import  App  from "./App";
+import App from './App';
 
-const rootElement = document.getElementById("root") as HTMLElement;
+const rootElement = document.getElementById('root') as HTMLElement;
 const root = createRoot(rootElement);
 
 root.render(
