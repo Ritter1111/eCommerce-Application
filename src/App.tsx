02179285e2277bd7a0c159f6137ed5d3ec040556
Angelina Liapin <angelina.liapin@gmail.com>
2023-08-02 12:46:15 +0300

    feat: add husky to project

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,15 @@ import React from 'react';
 const App = () => {
   return (
     <div>
-      <h1>Hello</h1>
+      <h1> <h4>fcdhdhf     <p>jdkjdk shdbf j        hds
+        
+        
+
+
+
+        <label htmlFor="dsj"></label     >
+        
+        </p></h4> Hello</h1>
     </div>
   )
 }
