1a915f87a1d4ef5586ad9c13c2309e2e21a04827
Yelyzaveta Vasylenko <liza.vasilenko13@gmail.com>
2023-08-14 15:14:38 +0200

    feat: update burger menu, add vars.css, update styles

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,10 +1,5 @@
-import React, { useState } from 'react';
-import {
-  AppBar,
-  Container,
-  Typography,
-  Box
-} from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { AppBar, Container, Typography, Box } from '@mui/material';
 import { Link, NavLink } from 'react-router-dom';
 import classes from './NavBar.module.css';
 import './pageLinks.css';
@@ -12,19 +7,38 @@ import { LOGIN_ROUTE, MAIN_ROUTE, REGISTRATION_ROUTE } from '../../utils/consts'
 import { ShoppingBag, LockOpen, Person, Menu, Close } from '@mui/icons-material';
 
 export default function NavBar() {
-  const [isMenuBtnCliked, setMenuBtnClicked] = useState(false);
+  const [isMenuOpen, setMenuOpen] = useState(false);
 
+  document.body.style.overflowY = `${isMenuOpen ? 'hidden' : 'auto'}`;
 
-  document.body.style.overflowY = `${isMenuBtnCliked ? 'hidden' : 'auto'}`;
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen)
+  }
+
+  // close burger menu if it was open and screen width >= 900
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 900 && isMenuOpen) {
+        closeMenu();
+      }
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [isMenuOpen]);
+
 
   return (
     <AppBar position="static" sx={{ backgroundColor: 'black' }} elevation={0}>
     <Container maxWidth="xl" sx={{ height: 63, display: 'flex', alignItems: 'center'}}>
       <Box sx={{ display: 'flex', justifyContent: 'space-between', flexGrow: 1, alignItems: 'center' }}>
-        <Link onClick={() => {
-          if (isMenuBtnCliked) setMenuBtnClicked(!isMenuBtnCliked)
-        }} to={MAIN_ROUTE} title="Go to Home" className={classes.link}>
-          <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <Link onClick={() => { if (isMenuOpen) closeMenu() }} to={MAIN_ROUTE} title="Go to Home" className={classes.link}>
+          <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', color: 'white' }}>
             <ShoppingBag sx={{ mr: 1 }} />
             <Typography
               variant="h6"
@@ -40,18 +54,18 @@ export default function NavBar() {
             </Typography>
           </Box>
         </Link>
-        <Box onClick={() => setMenuBtnClicked(!isMenuBtnCliked)} sx={{ display: { xs: 'block', md: 'none' } }}>
-          {isMenuBtnCliked ? <Close/> : <Menu/>}
+        <Box onClick={() => toggleMenu()} sx={{ display: { xs: 'block', md: 'none' } }}>
+          {isMenuOpen ? <Close/> : <Menu/>}
         </Box>
-        <Box className={`${isMenuBtnCliked ? classes.active : ''} ${classes.menu}`}>
-          <NavLink onClick={() => setMenuBtnClicked(!isMenuBtnCliked)} to={MAIN_ROUTE} className="pages__link" title="Go to Home">
+        <Box className={`${isMenuOpen ? classes.active : ''} ${classes.menu}`}>
+          <NavLink onClick={() => closeMenu()} to={MAIN_ROUTE} className="pages__link" title="Go to Home">
             Home
           </NavLink>
-          <NavLink onClick={() => setMenuBtnClicked(!isMenuBtnCliked)} to={LOGIN_ROUTE} className={classes.btn} title="Log In">
+          <NavLink onClick={() => closeMenu()} to={LOGIN_ROUTE} className={classes.btn} title="Log In">
             <LockOpen sx={{ mr: 0.5 }}/>
             Log In
           </NavLink>
-          <NavLink onClick={() => setMenuBtnClicked(!isMenuBtnCliked)} to={REGISTRATION_ROUTE} className={classes.btn} title="Sign Up">
+          <NavLink onClick={() => closeMenu()} to={REGISTRATION_ROUTE} className={classes.btn} title="Sign Up">
             <Person sx={{ mr: 0.5 }}/>
             Sign Up
           </NavLink>
