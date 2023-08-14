987b6b1c1b61bb7f346ca76437782a6d1c93c477
Yelyzaveta Vasylenko <liza.vasilenko13@gmail.com>
2023-08-14 19:02:05 +0200

    feat: implement logout

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import routes from '../utils/routes';
 import NotFound from '../views/not-found/notFound';
-import { LOGIN_ROUTE } from '../utils/consts';
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from '../utils/consts';
 import LogIn from '../views/authorization/log-in/LogIn';
-const token = localStorage.getItem('authToken');
+import { AuthContext } from '../context';
+import SignUp from '../views/authorization/sign-up/SignUp';
 
 const AppRouter = () => {
+  const { isAuth } = useContext(AuthContext);
   return (
     <Routes>
       {routes.map((route, index) => (
         <Route key={index} path={route.path} element={<route.element />} />
       ))}
       <Route
+        path={REGISTRATION_ROUTE}
+        element={isAuth ? <Navigate to="/"/> : <SignUp/>}
+      />
+      <Route
         path={LOGIN_ROUTE}
-        element={token ? <Navigate to="/" /> : <LogIn />}
+        element={isAuth ? <Navigate to="/" /> : <LogIn />}
       />
       <Route path="*" element={<NotFound />} />
     </Routes>
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,14 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import { AppBar, Container, Typography, Box } from '@mui/material';
+import React, { useContext, useEffect, useState } from 'react';
+import { AppBar, Container, Typography, Box, Button } from '@mui/material';
 import { Link, NavLink } from 'react-router-dom';
 import classes from './NavBar.module.css';
 import './pageLinks.css';
 import { LOGIN_ROUTE, MAIN_ROUTE, REGISTRATION_ROUTE } from '../../utils/consts';
-import { ShoppingBag, LockOpen, Person, Menu, Close } from '@mui/icons-material';
+import { ShoppingBag, LockOpen, Person, Menu, Close, Logout } from '@mui/icons-material';
+import { AuthContext } from '../../context';
+import routes from '../../utils/routes';
 
 export default function NavBar() {
   const [isMenuOpen, setMenuOpen] = useState(false);
-
+  const { isAuth, setIsAuth } = useContext(AuthContext);
   document.body.style.overflowY = `${isMenuOpen ? 'hidden' : 'auto'}`;
 
   const closeMenu = () => {
@@ -17,6 +19,11 @@ export default function NavBar() {
 
   const toggleMenu = () => {
     setMenuOpen(!isMenuOpen)
+  }
+
+  const logout = () => {
+    setIsAuth(false);
+    localStorage.clear();
   }
 
   // close burger menu if it was open and screen width >= 900
@@ -35,43 +42,50 @@ export default function NavBar() {
 
   return (
     <AppBar position="static" sx={{ backgroundColor: 'black' }} elevation={0}>
-    <Container maxWidth="xl" sx={{ height: 63, display: 'flex', alignItems: 'center'}}>
-      <Box sx={{ display: 'flex', justifyContent: 'space-between', flexGrow: 1, alignItems: 'center' }}>
-        <Link onClick={() => { if (isMenuOpen) closeMenu() }} to={MAIN_ROUTE} title="Go to Home" className={classes.link}>
-          <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', color: 'white' }}>
-            <ShoppingBag sx={{ mr: 1 }} />
-            <Typography
-              variant="h6"
-              noWrap
-              sx={{
-                mr: 2,
-                fontFamily: 'monospace',
-                fontWeight: 700,
-                letterSpacing: '.2rem',
-              }}
-            >
-              STORE
-            </Typography>
+      <Container maxWidth="xl" sx={{ height: 63, display: 'flex', alignItems: 'center'}}>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between', flexGrow: 1, alignItems: 'center' }}>
+          <Link onClick={() => { if (isMenuOpen) closeMenu() }} to={MAIN_ROUTE} title="Go to Home" className={classes.link}>
+            <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center', color: 'white' }}>
+              <ShoppingBag sx={{ mr: 1 }} />
+              <Typography
+                variant="h6"
+                noWrap
+                sx={{
+                  mr: 2,
+                  fontFamily: 'monospace',
+                  fontWeight: 700,
+                  letterSpacing: '.2rem',
+                }}
+              >
+                STORE
+              </Typography>
+            </Box>
+          </Link>
+          <Box onClick={() => toggleMenu()} sx={{ display: { xs: 'block', md: 'none' } }}>
+            {isMenuOpen ? <Close/> : <Menu/>}
           </Box>
-        </Link>
-        <Box onClick={() => toggleMenu()} sx={{ display: { xs: 'block', md: 'none' } }}>
-          {isMenuOpen ? <Close/> : <Menu/>}
+          <Box className={`${isMenuOpen ? classes.active : ''} ${classes.menu}`}>
+            {routes.map((route, index) => (
+              <NavLink key={index} onClick={() => closeMenu()} to={route.path} className="pages__link" title={route.title}>
+                {route.name}
+              </NavLink>
+            ))}
+            {isAuth ?
+              (<Button onClick={() => {logout(); closeMenu();}} className={classes.btn} sx={{color: 'black', textTransform: 'none'}}><Logout sx={{ mr: 0.5 }}/>Logout</Button>) :
+              (<>
+                <NavLink onClick={() => closeMenu()} to={LOGIN_ROUTE} className={classes.btn} title="Log In">
+                  <LockOpen sx={{ mr: 0.5 }}/>
+                  Log In
+                </NavLink>
+                <NavLink onClick={() => closeMenu()} to={REGISTRATION_ROUTE} className={classes.btn} title="Sign Up">
+                  <Person sx={{ mr: 0.5 }}/>
+                  Sign Up
+                </NavLink>
+              </>
+              )}
+          </Box>
         </Box>
-        <Box className={`${isMenuOpen ? classes.active : ''} ${classes.menu}`}>
-          <NavLink onClick={() => closeMenu()} to={MAIN_ROUTE} className="pages__link" title="Go to Home">
-            Home
-          </NavLink>
-          <NavLink onClick={() => closeMenu()} to={LOGIN_ROUTE} className={classes.btn} title="Log In">
-            <LockOpen sx={{ mr: 0.5 }}/>
-            Log In
-          </NavLink>
-          <NavLink onClick={() => closeMenu()} to={REGISTRATION_ROUTE} className={classes.btn} title="Sign Up">
-            <Person sx={{ mr: 0.5 }}/>
-            Sign Up
-          </NavLink>
-        </Box>
-      </Box>
-    </Container>
-  </AppBar>
+      </Container>
+    </AppBar>
   );
 }
