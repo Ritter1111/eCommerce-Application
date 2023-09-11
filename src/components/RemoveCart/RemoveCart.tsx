1c689100b9838fdf6b7cd2a3566353f30fb26d2c
Angelina Liapin <angelina.liapin@gmail.com>
2023-09-12 00:38:15 +0300

    fix: remove file

--- src/components/RemoveCart/RemoveCart.tsx
+++ src/components/RemoveCart/RemoveCart.tsx
@@ -1,12 +1,7 @@
 import { Link } from '@mui/material';
 import React from 'react';
-// import { removeBasket } from './Api-RemoveCart';
 
 export default function RemoveCart() {
-  // const handleRemoveCart = async () => {
-  //   // await removeBasket();
-  // };
-
   return (
     <Link component="button" sx={{ color: 'black', textDecoration: 'none' }}>
       Delete all items
