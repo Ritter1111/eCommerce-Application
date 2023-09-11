f06e97edb158010e57d537d8f68a322fa909e9c0
Angelina Liapin <angelina.liapin@gmail.com>
2023-09-11 20:34:46 +0300

    feat: add image for empty cart

--- src/views/basket/EmptyCart/EmptyCart.tsx
+++ src/views/basket/EmptyCart/EmptyCart.tsx
@@ -1,21 +1,25 @@
-import { ShoppingBag } from "@mui/icons-material";
 import { Box, Typography } from "@mui/material";
 import React from "react";
+import { Link } from "react-router-dom";
+import styles from './EmptyCard.module.css'
 
 export default function EmptyCartMessage() {
   return (
     <Box
       sx={{
-        mt: '40vh',
+        mt: '20vh',
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
       }}
     >
-      <ShoppingBag fontSize="large" sx={{ color: '#212121' }} />
+       <Box sx={{width: '280px'}} component="img" src="https://cdn.dribbble.com/users/351694/screenshots/10760065/media/c93e509e870484db932891b96447bd85.gif"/>
       <Typography align="center" variant="h6">
         Your cart is empty
       </Typography>
+      <Link to="/catalog" className={styles.link}>
+        continue shopping
+      </Link>
     </Box>
   );
 }
